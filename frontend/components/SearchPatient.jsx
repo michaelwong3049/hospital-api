e549183b5fcaf558b50c6979baa653420ced8c43
michaelwong3049 <michaelwong3049@gmail.com>
2024-06-02 11:53:44 -0400

    Added the SearchPatient form component

--- frontend/components/SearchPatient.jsx
+++ frontend/components/SearchPatient.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+
 function SearchPatient(patient) {
+    const [result, setResult] = useState(null);
+
     return(
-        <form>
-            <input type="text" id="searchPatientInput" name="searchPatient" placeholder="Please Enter Patient Name"></input>
-        </form>
+        <div className="searchPatient">
+            <form>
+                <input type="text" id="searchPatientInput" name="searchPatient" placeholder="Please Enter Patient Name"></input>
+                <input type="submit" id="submitPatient"></input>
+            </form>
+        </div>
     );
 }
 
-export default SearchPatient
+export default SearchPatient    
