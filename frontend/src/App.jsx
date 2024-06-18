17d583467275dcc8c0677d867955fd8c2068a660
michaelwong3049 <michaelwong3049@gmail.com>
2024-06-18 10:16:51 -0400

    Added ability to pass data between pages, and added new Routes to new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 //pages
 import Home from '../pages/Home'
-import Update from '../pages/Update'
+import ChoosePatient from '../pages/ChoosePatient'
 import Delete from '../pages/Delete'
 import Create from '../pages/Create'
+import Success from '../pages/Success'
+import Update from '../pages/Update'
 
 //components
 import Navbar from '../components/Navbar'
 
 function App() {
+  const [data, setData] = useState('');
+  const [name, setName] = useState('');
+
   return(
     <div className="App">
         <BrowserRouter>
@@ -17,9 +23,11 @@ function App() {
             <div className="pages">
                 <Routes>
                     <Route path="/" element={<Home />}/>
-                    <Route path="/Update" element={<Update />} />
-                    <Route path="Create" element={<Create />} />
+                    <Route path="/ChoosePatient" element={<ChoosePatient setData={setData} setName={setName}/>} />
+                    <Route path='/Update' element={<Update data={data} name={name}/>} />
+                    <Route path="Create" element={<Create />} />  
                     <Route path="/Delete" element={<Delete />} />
+                    <Route path="/Success" element={<Success />} />
                 </Routes>
             </div>
         </BrowserRouter>
